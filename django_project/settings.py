92b66c49b1948ae7a5aa48737c44a4eec52d3441
qqq99 <xuqiankarlie@gmail.com>
2021-02-22 13:06:11 +1100

    add video

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,10 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/image')
-MEDIA_URL = 'media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+''' MEDIA_ROOT = os.path.join(BASE_DIR, 'media/image')
+MEDIA_URL = 'media/' '''
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
--- django_project/views.py
+++ django_project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 class HomePage(TemplateView):
     template_name = 'home.html'
